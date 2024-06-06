2d5c7963f5c8eb86b9f1fcdba445e494910b7af7
Narciso Castro <narcisocastro0802@gmail.com>
2024-06-05 17:56:43 -0700

    Added if and else if statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+
+           Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            var favNumber = r.Next(1, 1000);
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine($"You guessed it!!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too low");
+            }
+            else
+            {
+                Console.WriteLine($"Too High");
+            }
         }
     }
 }
